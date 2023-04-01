c4469aa383541b6a87510184caa0915741a4d459
Vedika Rotti <vedika.rotti@gmail.com>
2023-04-01 13:34:52 +0530

    Enahance-chatgpt

---
+++ index.js
@@ -0,0 +1,36 @@
+const { Configuration, OpenAIApi } = require("openai");
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const configuration = new Configuration({
+  organization: "org-hLpGP4JmzpitL0LccnVkrien",
+  apiKey: "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",
+});
+const openai = new OpenAIApi(configuration);
+
+// add body parser and corrs to express
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+const port = 3080;
+
+app.post("/", async (req, res) => {
+  const { message } = req.body;
+  console.log(message, "message");
+  const response = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: `${message}`,
+    max_tokens: 100,
+    temperature: 0.5,
+  });
+  res.json({
+    message: response.data.choices[0].text,
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server app listening at http://localhost:${port}`);
+});
